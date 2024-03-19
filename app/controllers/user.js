4d21e5b96174234d4c1002243c98fcb05d6ede64
Enzo91080 <107677982+Enzo91080@users.noreply.github.com>
2024-03-19 17:19:28 +0100

    Activity13 is finished

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,8 +1,10 @@
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
   try {
     const userData = req.body;
+    userData.password = await bcrypt.hash(userData.password, 10);
     const user = await User.create(userData);
     res.status(201).send(user);
   } catch (error) {
